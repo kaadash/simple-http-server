11b37d44f0a487b6fbe4a22b8cac2477bda6fe14
Ronisko <koripier@gmail.com>
2016-12-02 18:06:47 +0100

    add HEAD request

--- src/Sender.java
+++ src/Sender.java
@@ -1,3 +1,5 @@
+import javafx.application.Platform;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -10,7 +12,7 @@ public class Sender {
                 sendGET(url, port, file);
                 break;
             case 1:
-
+                sendHEAD(url, port, file);
                 break;
             case 2:
                 break;
@@ -38,7 +40,25 @@ public class Sender {
         bufRead.close();
         wtr.close();
     }
+    private static void sendHEAD(String url, int port, String file) throws Exception {
+        System.out.println("Sending HEAD request.\nResponse:\n");
+        Socket soc = new Socket(url, port);
 
+        PrintWriter wtr = new PrintWriter(soc.getOutputStream());
+
+        wtr.println("HEAD /" + file + " HTTP/1.1");
+        wtr.flush();
+
+        BufferedReader bufRead = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+        String response;
+
+        while((response = bufRead.readLine()) != null){
+            System.out.println(response);
+        }
+
+        bufRead.close();
+        wtr.close();
+    }
     private static void sendDELETE(String url, int port, String file) throws Exception {
         System.out.println("Sending DELETE request.");
         Socket soc = new Socket(url, port);
